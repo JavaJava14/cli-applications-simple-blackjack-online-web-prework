4fad0403370971f175310521e3d33d295ba0ad12
Jose Chavira <joeychavira2016@gmail.com>
2019-09-03 07:23:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,6 +34,10 @@ def initial_round
 end
 
 def hit?
+  valid_inputs = ["h", "s"]
+
+  prompt_user
+  user_input = get_user_input
    if user_input == "h"
      card_total += deal_card
    elsif user_input == "s"
